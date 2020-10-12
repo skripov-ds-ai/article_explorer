174d235301f1b324d6c4212058cab69ad90d99d8
Denis Skripov <nizhikebinesi@gmail.com>
2020-10-13 00:12:33 +0600

    fix db and config

--- config.py
+++ config.py
@@ -2,19 +2,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+POSTGRES_USER = os.getenv('POSTGRES_USER', None)
+POSTGRES_PW = os.getenv('POSTGRES_PW', None)
+POSTGRES_URL = os.getenv('POSTGRES_URL', None)
+POSTGRES_DB = os.getenv('POSTGRES_DB', None)
 
-POSTGRES_USER = 'navigator'
-POSTGRES_PW = 'qwerty2'
-POSTGRES_URL = "127.0.0.1:5432"
-POSTGRES_DB = "explorer"
-
-
-# DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-#         user=POSTGRES_USER,
-#         pw=POSTGRES_PW,
-#         url=POSTGRES_URL,
-#         db=POSTGRES_DB
-# )
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_shall_not_pass'
@@ -24,7 +16,5 @@ class Config:
         url=POSTGRES_URL,
         db=POSTGRES_DB
     )
-    #os.environ.get('DATABASE_URL')# or \
-                              # 'sqlite:///' + os.path.join(basedir, 'explorer.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
